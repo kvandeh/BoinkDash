0d53492a9bcab68fcb31f1b5e3fcfb1d17cb024e
Ferret <43520027+flowingferret3@users.noreply.github.com>
2023-06-08 12:46:02 +0200

    Add files via upload

---
+++ __main__.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, send_file
+
+app = Flask("BoinkDask")
+
+@app.route("/")
+async def main():
+    print("f")
+    return render_template("index.html")
+
+@app.route("/static/<file>")
+async def files(file):
+    return send_file("./static/"+file)
+
+app.run(debug=True, host="0.0.0.0", port=80)
